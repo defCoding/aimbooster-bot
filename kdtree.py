a02ed7792cbe85b06b724ad6a85332dbac98d80f
Kevin Cao <kcao1998@gmail.com>
2020-11-10 03:11:13 -0500

    kdtree implementation beginning

---
+++ kdtree.py
@@ -0,0 +1,8 @@
+class KDNode:
+    def __init__(self, data=None, axis=None, left=None, right=None):
+        self.data = data
+        self.axis = axis
+        self.left = left
+        self.right = right
+
+    
